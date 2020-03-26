dd385f0a4791d61d238cb7c35cae2fe221e905fc
Anthony Bui <anthonybui@gmail.com>
2020-03-26 15:39:22 -0700

    Update SHA

--- Formula/ahead.rb
+++ Formula/ahead.rb
@@ -2,7 +2,7 @@ class Ahead < Formula
   desc "STOOPID Calendar preview written in Rust"
   homepage "https://github.com/epylinkn/ahead"
   url "https://github.com/epylinkn/ahead/archive/v0.1.0.tar.gz"
-  sha256 "9e85e5df1c4144459d9551fc3687f9edd8dec242f503f031f10216f08acffd9a"
+  sha256 "415a1d6a26d4d2c021154a99790ca916b914bdb8852b7102a60e89a0fa9724b1"
   version "0.1.0"
 
   depends_on "ical-buddy"
