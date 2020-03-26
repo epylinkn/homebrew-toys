440cb2f7ec36b695e1247c6cf99ee32d2344ebd3
Anthony Bui <anthonybui@gmail.com>
2020-03-26 15:26:08 -0700

    fixes

--- Formula/ahead.rb
+++ Formula/ahead.rb
@@ -2,8 +2,10 @@ class Ahead < Formula
   desc "STOOPID Calendar preview written in Rust"
   homepage "https://github.com/epylinkn/rust-ahead"
   url "https://github.com/epylinkn/rust-ahead/archive/v0.1.0.tar.gz"
-  sha256 "8095de66a6f24b950cb88133c8254b7026ce77176c1675ebcf94ec9690f17ea4"
+  sha256 "9e85e5df1c4144459d9551fc3687f9edd8dec242f503f031f10216f08acffd9a"
   version "0.1.0"
+
+  depends_on "ical-buddy"
 
   def install
     bin.install "ahead"
