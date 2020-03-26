1cac75eea6013d094b561f261291ce9695b2555c
Anthony Bui <anthonybui@gmail.com>
2020-03-26 15:37:51 -0700

    :bug:fix: Maybe?

--- Formula/ahead.rb
+++ Formula/ahead.rb
@@ -1,7 +1,7 @@
 class Ahead < Formula
   desc "STOOPID Calendar preview written in Rust"
-  homepage "https://github.com/epylinkn/rust-ahead"
-  url "https://github.com/epylinkn/rust-ahead/archive/v0.1.0.tar.gz"
+  homepage "https://github.com/epylinkn/ahead"
+  url "https://github.com/epylinkn/ahead/archive/v0.1.0.tar.gz"
   sha256 "9e85e5df1c4144459d9551fc3687f9edd8dec242f503f031f10216f08acffd9a"
   version "0.1.0"
 
